6682e7f04f1019808cbc840922aab22dcc0d5a81
Valerie Moore <periwinkle17@outlook.com>
2018-06-13 23:28:52 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -128,5 +128,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+adding_to_dennis = 
+  programmer_hash[:dennis_ritchie][:languages] = "C", "Assembly"
+  programmer_hash   
 end
